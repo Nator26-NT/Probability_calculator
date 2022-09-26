632aab70607de0110c4a85081245f896b373b051
Nator26-NT <111059939+Nator26-NT@users.noreply.github.com>
2022-09-26 12:48:39 +0200

    Add files via upload

---
+++ PROBABILITY_CALCULATOR.py
@@ -0,0 +1,53 @@
+print("PROBABILITY_CALCULATOR")
+
+
+print("Weather Forcast ================")
+
+
+Todays_temp = float(input("What is todays Temperature : "))
+
+if Todays_temp == "20":
+	print("grab  a jersey on your way out  it will not be that hot")
+elif Todays_temp == "21":
+	print("wear a light jersey it wont be that hot")
+	
+elif Todays_temp == "22":
+	print("grab  a jersey it will not be that hot")
+	
+elif Todays_temp == "23":
+	print("grab  a jersey it will not be that hot")
+
+elif Todays_temp == "24":
+	print("grab  a jersey it will not be that hot")
+
+elif Todays_temp == "25":
+	print("to hot to be wearing long fuzz stuff put on a Tshirt with a short")
+	
+elif Todays_temp == "26":
+	print("the weather is extreme go to tbe beach or hit the pool")
+	
+elif Todays_temp == "27":
+	print("stay in the shadows the weather is not that human")
+
+elif Todays_temp == "28":
+	print("grab some sun screen to protect ur skin ")
+
+elif Todays_temp == "29":
+	print("low possible chance of raining")
+
+elif Todays_temp == "30":
+	print("low chance of raining")
+
+elif  Todays_temp == "31":
+	print("medium chance of raining grab an umbrella on ur way out")
+	
+elif Todays_temp ==  "32":
+	print("its going to be raining today")
+
+else : 
+      print("What a great temperature have  good day :-} ")
+	
+
+
+
+
